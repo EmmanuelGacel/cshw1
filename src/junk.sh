f9e17d8dc9005ebe9824cc3eb9082400384c8fe5
krw2146@ap.cs.columbia.edu <krw2146@ap.cs.columbia.edu>
2023-01-28 16:34:33 -0500

    command line logic

--- src/junk.sh
+++ src/junk.sh
@@ -5,4 +5,23 @@ Emmanuel and Katies Recyclying Bin
 Name: junk.sh
 #####################################################
 
-#test git push capabilities#
+#test git push capabilities
+
+
+function error() {
+	echo Usage junk.sh [hlp] [list of files]
+	echo -h: Display help.
+	echo -l: List junked files.
+	echo -p: Purge all files.
+	echo [list all arguments to junk those files.
+}
+
+if[-z "$1"]; then
+	error
+elif ["$1" == "-h" || "$1" == "-l" || "$1" == "-p"] % when theres a hlp input
+	%do something
+elif
+	%do something with file name
+fi
+
+
