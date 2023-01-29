c4bd46a2d362cb165de61b05e6daaf5b27d669d4
krw2146@ap.cs.columbia.edu <krw2146@ap.cs.columbia.edu>
2023-01-29 13:16:03 -0500

    getopts portion

--- src/junk.sh
+++ src/junk.sh
@@ -1,27 +1,42 @@
 #!/bin/bash
 
 #####################################################
-Emmanuel and Katies Recyclying Bin
-Name: junk.sh
+#Name: junk.sh
 #####################################################
 
 #test git push capabilities
 
 
-function error() {
-	echo Usage junk.sh [hlp] [list of files]
-	echo -h: Display help.
-	echo -l: List junked files.
-	echo -p: Purge all files.
-	echo [list all arguments to junk those files.
+function description() {
+	FOURSPACE='    '
+	echo Usage junk.sh [hlp] [list of files]$'\n'"${FOURSPACE}"-h: Display help.$'\n'"${FOURSPACE}"-l: List junked files.$'\n'"${FOURSPACE}"-p: Purge all files.$'\n'"${FOURSPACE}"[list all arguments] to junk those files.
 }
 
-if[-z "$1"]; then
-	error
-elif ["$1" == "-h" || "$1" == "-l" || "$1" == "-p"] % when theres a hlp input
-	%do something
-elif
-	%do something with file name
+if [ -z "$1" ];then #there were no arguments supplied
+	description
+	exit 1
 fi
 
+while getopts ":hlp" option; do
+	case "$option" in
+		h)
+			description ;;
+		l)
+			listAll ;;
+		p)
+			purgeAll ;;
+		?)
+			echo Error: Unknown option -${OPTARG}.
+			description
+			exit 1 ;;
+	esac
+done
 
+function listAll(){
+	echo will list all the files in recycle bin
+}
+function purgeAll(){
+	echo will purge all the files in recycle bin
+}
+
+# NEED TO CONSIDER CASE WHEN FILE ARUGMENTS ARE ENTERED
